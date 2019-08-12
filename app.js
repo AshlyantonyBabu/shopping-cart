c8f776458dc2b79fc938e51f064a61b86f5a55fc
AshlyantonyBabu <elsyantonybabu@gmail.com>
2019-08-12 22:56:37 +0530

    cart

---
+++ app.js
@@ -0,0 +1,17 @@
+var exp=require('express')
+const app= exp()
+const path=require('path')
+app.use(exp.static(path.join(__dirname,"/public")))
+app.set("view engine","ejs")
+app.set("views","./src/view")
+app.get("/",function(req,res){
+    res.render("index",{nav:[{link:"/",title:"HOME"},{link:"/login",title:"LOGIN"},{link:"/shop",title:"SHOP"}]})
+})
+app.listen(4000,function(req,res){
+    console.log("server is ready.......")
+
+})
+app.get("/login",function(req,res){
+    
+    res.render("login",{nav:[{link:"/",title:"HOME"},{link:"/login",title:"LOGIN"},{link:"/shop",title:"SHOP"}]})
+})
