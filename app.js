c29f8244f89c7a54a3e4869f1b43bc36839e4ceb
AshlyantonyBabu <elsyantonybabu@gmail.com>
2019-08-18 19:23:15 +0530

    add product

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path=require('path')
 app.use(exp.static(path.join(__dirname,"/public")))
 app.set("view engine","ejs")
 app.set("views","./src/view")
+
+var rrt=require("./router/adminrouter")
+app.use("/admin",rrt)
+
 app.get("/",function(req,res){
     res.render("index",{nav:[{link:"/",title:"HOME"},{link:"/login",title:"LOGIN"},{link:"/shop",title:"SHOP"}]})
 })
@@ -12,6 +16,9 @@ app.listen(4000,function(req,res){
 
 })
 app.get("/login",function(req,res){
-    
+
     res.render("login",{nav:[{link:"/",title:"HOME"},{link:"/login",title:"LOGIN"},{link:"/shop",title:"SHOP"}]})
+})
+app.get("/register",function(req,res){
+    res.render("register",{nav:[{link:"/",title:"HOME"},{link:"/login",title:"LOGIN"},{link:"/shop",title:"SHOP"}]})
 })
---
+++ model/additem.js
@@ -0,0 +1,10 @@
+var mong=require('mongoose')
+var addschema=mong.Schema
+var itemschema=new addschema({
+    itemname:{type:String,required:true},
+    itemprice:{type:Number,required:true},
+    itemqty:{type:Number,required:true}
+    
+})
+var itemmodel=mong.model("items",itemschema,"additem")
+module.exports=itemmodel
